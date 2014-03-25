282cd4c5b9190495c3528eb23d90e0b6176936b7
Luqi Pan <luqi.pan@gmail.com>
2014-03-25 00:19:55 -0400

    Remove Element- utilize the requirement, a simpler version

--- RemoveElement.java
+++ RemoveElement.java
@@ -42,3 +42,18 @@ public class Solution {
         return left;
     }
 }
+
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        if (A.length == 0) {
+            return 0;
+        }
+        int index = 0;
+        for (int i = 0 ; i < A.length ; i++) {
+            if (A[i] != elem) {
+                A[index++] = A[i];
+            }
+        }
+        return index;
+    }
+}
