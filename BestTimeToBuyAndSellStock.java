29a9b197f0cc3ea70305353118968405bc4a522e
Luqi Pan <luqi.pan@gmail.com>
2014-03-25 01:25:10 -0400

    Best Time to Buy and Sell Stock- simplified version

--- BestTimeToBuyAndSellStock.java
+++ BestTimeToBuyAndSellStock.java
@@ -22,3 +22,19 @@ public class Solution {
         return maxProfit;
     }
 }
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len == 0) {
+            return 0;
+        }
+        int min = prices[0];
+        int profit = 0;
+        for (int i = 0 ; i < len ; i++) {
+            min = Math.min(min, prices[i]);
+            profit = Math.max(profit, prices[i] - min);
+        }
+        return profit;
+    }
+}
