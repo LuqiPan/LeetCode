ede35244bfd8f049a08d949cf68ac1caa347dd71
Luqi Pan <luqi.pan@gmail.com>
2014-03-26 18:06:39 -0400

    Add Two Numbers- simpler version

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -78,3 +78,29 @@ public class Solution {
         return dummyHead.next;
     }
 }
+
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        int carry = 0;
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = dummyHead;
+        while (l1 != null || l2 != null || carry != 0) {
+            int v1 = (l1==null)?0:l1.val;
+            int v2 = (l2==null)?0:l2.val;
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            
+            ListNode temp = new ListNode(sum);
+            p.next = temp;
+            p = p.next;
+            l1 = (l1==null)?null:l1.next;
+            l2 = (l2==null)?null:l2.next;
+        }
+        return dummyHead.next;
+    }
+}
+
